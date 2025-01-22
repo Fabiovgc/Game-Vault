bf309c239d328ec01c678518f563bf9e36d6091f
FabioVGC <fabiovgc@gmail.com>
2025-01-22 19:06:23 -0300

    feat: add Game Seed

--- src/main/java/com/devsuperior/gamevault/entities/Game.java
+++ src/main/java/com/devsuperior/gamevault/entities/Game.java
@@ -27,25 +27,32 @@ public class Game {
 
         private String genre;
 
-        private String platform;
+        private String platforms;
+
+        private Double score;
 
         private String imgUrl;
 
+        // definindo propriedades "longDescription" e "shortDescription" como texto, não VARCHAR (255 caracteres),
+
+        @Column(columnDefinition = "TEXT")
         private String shortDescription;
 
+        @Column(columnDefinition = "TEXT")
         private String longDescription;
 
 
         // Constructor para instanciar a classe 
 
         public Game(Long id, String title, Integer year,
-         String genre, String platform, String imgUrl, 
+         String genre, String platforms, Double score, String imgUrl, 
          String shortDescription, String longDescription){
                 this.id = id;
                 this.title = title;
                 this.year = year;
                 this.genre = genre;
-                this.platform = platform;
+                this.platforms = platforms;
+                this.score = score;
                 this.imgUrl = imgUrl;
                 this.shortDescription = shortDescription;
                 this.longDescription = longDescription;
@@ -95,13 +102,22 @@ public class Game {
         }
 
 
-        public String getPlatform() {
-                return platform;
+        public String getPlatforms() {
+                return platforms;
         }
 
 
-        public void setPlatform(String platform) {
-                this.platform = platform;
+        public void setPlatforms(String platforms) {
+                this.platforms = platforms;
+        }
+
+        public Double getScore() {
+                return score;
+        }
+
+
+        public void setScore(Double score) {
+                this.score = score;
         }
 
 
