90fd4b57704a9558f86b075a912f2603b99a88de
FabioVGC <fabiovgc@gmail.com>
2025-01-23 18:21:30 -0300

    feat: add Get game by id

--- src/main/java/com/devsuperior/gamevault/controllers/GameController.java
+++ src/main/java/com/devsuperior/gamevault/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.gamevault.dto.GameDTO;
 import com.devsuperior.gamevault.dto.GameMinDTO;
 import com.devsuperior.gamevault.services.GameService;
 
@@ -22,4 +24,13 @@ public class GameController {
                 List<GameMinDTO> dto = gameService.findAll();
                 return dto;
         }
+
+        // Annotation @PathVariable é usada para fazer a ligação entre a Id que será enviada e a Id passada neste método
+        // É preciso definir a URI (o endpoint) usando a sintaxe abaixo quando existem mais de um metodo HTTP GET na mesma classe Controller
+        @GetMapping(value = "/{id}")
+        public GameDTO findById(@PathVariable Long id){
+                GameDTO result = gameService.findById(id);
+                return result;
+        }
+
 }
--- src/main/java/com/devsuperior/gamevault/services/GameService.java
+++ src/main/java/com/devsuperior/gamevault/services/GameService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.gamevault.dto.GameDTO;
 import com.devsuperior.gamevault.dto.GameMinDTO;
 import com.devsuperior.gamevault.entities.Game;
 import com.devsuperior.gamevault.repositories.GameRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 // Registrando a classe Service como um componente do sistema (necessário na classe service ou outras classes "comuns")
 
@@ -24,10 +26,20 @@ public class GameService {
         // Criando uma "variavel" do tipo List<Game> chamada result / armazenar o valor de gameRepository / usar o método da sua classe pai (JpaRepository) "findAll()"
         // Criando uma "variavel" do tipo List<GameMinDTO> chamada dto / armazenar o valor de result/ usando o metodo .map() para alterar o valor de result para List<GameMinDTO>
         // Metodo retorna o valor de result que é uma variável do tipo List<GameMinDTO> ( o método pai deve corresponder)
+        // @transactional define o comportamento transacional de métodos ou classes
+        @Transactional(readOnly = true)
         public List<GameMinDTO> findAll(){
                 List<Game> result = gameRepository.findAll();
                 return result.stream().map(x -> new GameMinDTO(x)).toList();
+        }
 
+        // O metodo findById de JpaRepository retorna um dado do tipo Optional<> então é preciso adicionar à classe Repository
+        // Usando .get() para retornar um dado do tipo Optional<>
+        // Instanciando o constructor de GameDTO e passando a variavel result como argumento
+        @Transactional(readOnly = true)
+        public GameDTO findById(Long id){
+                Game result = gameRepository.findById(id).get();
+                return new GameDTO(result);
         }
 
 }
